7155231c592cfce1cdce76cc08eb9064dbba0638
Black_Baroness <shoyurash2@gmail.com>
2020-08-26 16:05:38 +0300

    gachi

--- src/main/java/bara/sapphire/UUIDManager.java
+++ src/main/java/bara/sapphire/UUIDManager.java
@@ -7,19 +7,23 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-
 public class UUIDManager implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(sender.hasPermission("sapphire.admin")) {
-            if(args.length <= 1) { sender.sendMessage(ChatColor.RED + "Введите Ник-нейм игрока"); } else {
+        if (sender.hasPermission("sapphire.admin")) {
+            if (args.length <= 1) {
+                sender.sendMessage(ChatColor.RED + "Введите ник игрока");
+            } else {
                 Player target = Bukkit.getPlayer(args[0]);
-                if(target == null) { sender.sendMessage(ChatColor.RED + "Игрок не зарегистрирован или не Онлайн"); } else {
+                if (target == null) {
+                    sender.sendMessage(ChatColor.RED + "Игрок не зарегистрирован или не онлайн");
+                } else {
                     sender.sendMessage(ChatColor.GREEN + target.getUniqueId().toString() + " - " + target.getName());
                 }
             }
-        } else { sender.sendMessage(ChatColor.RED + "У вас недостаточно прав для выполнения данной команды"); }
-
+        } else {
+            sender.sendMessage(ChatColor.RED + "У вас недостаточно прав для выполнения данной команды");
+        }
         return true;
     }
 }
